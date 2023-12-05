af9d4d3405e66d39c522ecf302bc7317cf598878
Bruno Coelho <bruno100vc@hotmail.com>
2023-12-05 20:30:12 +0100

    Add error checking attempt in sonar3

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import { caresRouter } from './routers/cares.routes.js';
+// Import { caresRouter } from './routers/cares.routes.js';
 import createDebug from 'debug';
 
 import { errorMiddleware } from './middleware/error.middleware.js';
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/cares', caresRouter); // Todo que empiece por /cares lo redirige a caresRouter
+// App.use('/cares', caresRouter); // Todo que empiece por /cares lo redirige a caresRouter
 app.use('/users', usersRouter); // Todo que empiece por /users lo redirige a usersRouter
 
 app.use(errorMiddleware);
